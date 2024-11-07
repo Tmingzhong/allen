f4aad4d6c3553c2c89405de4c64063f26d2465e4
Tmingzhong <taxe_m@126.com>
2024-11-07 22:53:12 +0800

    feat:modify

--- src/components/BeforeLogin/index.tsx
+++ src/components/BeforeLogin/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
-
 const BeforeLogin: React.FC = () => {
+
   return (
     <div>
       <p>
-        <b>Welcome to your dashboard!</b>
-        {' This is where site admins will log in to manage your website.'}
+        <b>欢迎登录！</b>
+        {' 这是管理后台。'}
       </p>
     </div>
   )
 }
 
 export default BeforeLogin
+
+
--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,12 +1,20 @@
+/*
+ * @Author: tang.haoming
+ * @Date: 2024-10-26 04:20:58
+ * @LastEditors: tang.haoming
+ * @LastEditTime: 2024-11-07 21:24:06
+ * @FilePath: /allen/src/components/Logo/Logo.tsx
+ * @Description: 
+ */
 import React from 'react'
 
 export const Logo = () => {
   return (
     /* eslint-disable @next/next/no-img-element */
     <img
-      alt="Payload Logo"
+      alt="aizhai Logo"
       className="max-w-[9.375rem] invert dark:invert-0"
-      src="https://raw.githubusercontent.com/payloadcms/payload/main/packages/payload/src/admin/assets/images/payload-logo-light.svg"
+      src="/../../../public/logo.png"
     />
   )
 }
